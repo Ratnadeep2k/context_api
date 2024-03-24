3a9bcdde5208544a067f49817f22d872ff1917e2
Ratnadeep Baruah <ratnadeepbaruah@Ratnadeeps-MacBook-Air.local>
2024-03-24 14:19:44 +0530

    components

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-
-import UserContext from "./userContext";
-
-const UserContextProvider =({children})=>{
-    const [user,setUser ]= React.useState(null)
-
-
-    return (
-        <UserContext.Provider value ={{user,setUser}}>
+import UserContext from '../context/userContext'
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
         {children}
         </UserContext.Provider>
     )
 }
+
 export default UserContextProvider
